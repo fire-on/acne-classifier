df75233c30d0776e695519857fac1a7fc937ab29
fire-on <148673903+fire-on@users.noreply.github.com>
2025-06-06 17:28:17 +0900

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import hf_hub_download
 import os
 
 model_path = hf_hub_download(
-    repo_id="your-username/your-model-repo",
-    filename="model.pth",
+    repo_id="whii/Swin-Transformer-Pretrained_multilabel-acne",
+    filename="99best_acne_swin.pth",
     token=os.environ["HF_TOKEN"]
 )
