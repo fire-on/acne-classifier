877d3bbb8c12d336fbc3f6634e8ca759cffd1043
fire-on <148673903+fire-on@users.noreply.github.com>
2025-06-06 17:06:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ print(device)
 try:
     model_path = hf_hub_download(
         repo_id="whii/Swin-Transformer-Pretrained_multilabel-acne",
-        filename="model.pth",
+        filename="99best_acne_swin.pth",
         token=os.environ["HF_TOKEN"],
         local_files_only=True
     )
