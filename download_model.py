0e36a52fd4725cac8ec92b86d76144f4f1c2d285
fire-on <148673903+fire-on@users.noreply.github.com>
2025-06-10 08:45:03 +0900

    Delete download_model.py

--- download_model.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import hf_hub_download
-import os
-
-model_path = hf_hub_download(
-    repo_id="whii/Swin-Transformer-Pretrained_multilabel-acne",
-    filename="99best_acne_swin.pth",
-    token=os.environ["HF_TOKEN"]
-)
